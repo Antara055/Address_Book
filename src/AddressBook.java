5ab859ca54039b4290d7fbf906062a5a4c955a65
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-21 01:36:39 +0530

    Added: new people

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,23 @@
+
 public class AddressBook {
     public static void main(String[] args){
         System.out.println("WELCOME IN ADDRESS BOOK PROGRAM");
+    }
+}class AddPerson extends AddressBook{
+
+    public void addName(String firstName, String lastName){
+        System.out.println("ENTERED FIRST NAME="+firstName);
+        System.out.println("ENTERED LAST NAME="+lastName);
+    }
+
+    public void addAddress(String address, String city, String state, int zipCode){
+        System.out.println("ADDRESS::"+address);
+        System.out.println("CITY::"+city);
+        System.out.println("STATE::"+state);
+        System.out.println("ZIP CODE::"+zipCode);
+    }
+    public void addContact(int phoneNo,String eMail){
+        System.out.println("PHONE NUMBER:"+phoneNo);
+        System.out.println("EMAIL:"+eMail);
     }
 }
