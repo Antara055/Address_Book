bca12c4e46eeaa55bf32ebcb4a1d93dc033678d9
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-26 17:25:19 +0530

    Added : add new contact

--- src/com/bridgelab/AddressBook.java
+++ src/com/bridgelab/AddressBook.java
@@ -1,37 +1,16 @@
 package com.bridgelab;
 
 import java.util.Scanner;
+import java.util.concurrent.Callable;
 
 public class AddressBook {
 
     public static void main(String[] args){
         System.out.println("WELCOME IN ADDRESS BOOK PROGRAM");
-        Scanner sc = new Scanner(System.in);
-
         System.out.println("Add new contact details to Address_Book");
+        AddContact addContact=new AddContact();
+        addContact.add();
 
-        System.out.println("Enter first-name : ");
-        String firstname = sc.nextLine();
-
-        System.out.println("Enter last-name : ");
-        String lastname = sc.nextLine();
-
-        System.out.println("Enter e-mail : ");
-        String email = sc.nextLine();
-
-        System.out.println("Enter Phone number : ");
-        long phone = sc.nextLong();
-
-        System.out.println("Enter city : ");
-        String city = sc.nextLine();
-
-        System.out.println("Enter state : ");
-        String state = sc.nextLine();
-
-        System.out.println("enter Zip-code : ");
-        int zip = sc.nextInt();
-
-        Contact_info contact=new Contact_info(firstname,lastname,email,phone,city,state,zip);
     }
 
 }
--- src/com/bridgelab/Contact_info.java
+++ src/com/bridgelab/Contact_info.java
@@ -1,5 +1,7 @@
 package com.bridgelab;
 
+import java.util.Scanner;
+
 public class Contact_info {
     String first_name;
     String last_name;
@@ -9,16 +11,17 @@ public class Contact_info {
     String state;
     int zip;
 
-    public Contact_info(String first_name,String last_name,String email,long phone_no,String city,String state,int zip){
-        this.first_name=first_name;
-        this.last_name=last_name;
-        this.email=email;
-        this.phone_no=phone_no;
-        this.city=city;
-        this.state=state;
-        this.zip=zip;
+    public Contact_info(String first_name, String last_name, String email, long phone_no, String city, String state, int zip) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.phone_no = phone_no;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
 
     }
 
 
 }
+
