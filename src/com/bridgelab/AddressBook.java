c68cbe2f9d0058b209d81db087cf75e10728edb8
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-27 14:07:05 +0530

    Added : new Contact

--- src/com/bridgelab/AddressBook.java
+++ src/com/bridgelab/AddressBook.java
@@ -1,17 +1,76 @@
 package com.bridgelab;
 
 import java.util.Scanner;
-import java.util.concurrent.Callable;
+
 
 public class AddressBook {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("WELCOME IN ADDRESS BOOK PROGRAM");
         System.out.println("Add new contact details to Address_Book");
-        AddContact addContact=new AddContact();
-        addContact.add();
+        Contact_info contact_info = new Contact_info();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Name: ");
+        contact_info.setName(sc.nextLine());
+        System.out.println("Enter Email: ");
+        contact_info.setEmail(sc.nextLine());
+        System.out.println("Enter Phone Number: ");
+        contact_info.setPhoneNumber(sc.nextLong());
+        System.out.println("Enter City: ");
+        contact_info.setCity(sc.nextLine());
+        System.out.println("Enter State: ");
+        contact_info.setState(sc.nextLine());
+        System.out.println("Enter Zip Pin: ");
+        contact_info.setZip(sc.nextInt());
+
+        System.out.println(" First Name: "+contact_info.getName()+
+                "\n Email-id: "+contact_info.getEmail()+
+                "\n Cell Num: "+contact_info.getPhoneNumber()+
+                "\n City: "+contact_info.getCity()+
+                "\n State: "+contact_info.getState()+
+                "\n Pin: "+contact_info.getZip());
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+       /* System.out.println(c1);
+        System.out.println(contact_info);
+    }
+}
+        System.out.println("Enter name : ");
+        addContact.setName(Scanner.nextLine());
+
+        System.out.println("Enter E-mail : ");
+        String email=sc.next();
+
+        System.out.println("Enter Phone number :");
+        long phone = sc.nextLong();
+
+        System.out.println("Enter city : ");
+        String city =sc.nextLine();
+
+        System.out.println("Enter state : ");
+        String state =sc.nextLine();
+
+        System.out.println("enter Zip-code : ");
+        int zip =sc.nextInt();
+        Contact_info contact_info= new Contact_info(name,email,phone,city,state,zip);
+
 
     }
 
 }
-
+*/
--- src/com/bridgelab/Contact_info.java
+++ src/com/bridgelab/Contact_info.java
@@ -3,25 +3,79 @@ package com.bridgelab;
 import java.util.Scanner;
 
 public class Contact_info {
-    String first_name;
-    String last_name;
+    //Variables
+    String name;
     String email;
     long phone_no;
     String city;
     String state;
     int zip;
 
-    public Contact_info(String first_name, String last_name, String email, long phone_no, String city, String state, int zip) {
-        this.first_name = first_name;
-        this.last_name = last_name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phone_no;
+    }
+
+    public void setPhoneNumber(long phone_no) {
+        this.phone_no = phone_no;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+}
+
+
+
+   /* @Override
+    public String toString() {
+        return "AddressBook \n [Name=" + name +",\n phoneNumber=" + phone_no + ", email=" + email +
+                ",\n city="+ city + ",\n state=" + state + ", zip=" + zip +  "]";
+    }
+
+    Using constructor to assign the values to the variables
+
+    public Contact_info(String name, String email, long phone_no, String city, String state, int zip) {
+        this.name = name;
         this.email = email;
         this.phone_no = phone_no;
         this.city = city;
         this.state = state;
         this.zip = zip;
-
     }
-
-
-}
+    */
 
