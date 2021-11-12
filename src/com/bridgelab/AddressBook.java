2258d074a41edcbdc2fd0060ae4c53fc941779be
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-12 19:36:48 +0530

    Added : multiple user details

--- src/com/bridgelab/AddressBook.java
+++ src/com/bridgelab/AddressBook.java
@@ -1,19 +1,25 @@
 package com.bridgelab;
 
-import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Scanner;
 /*@ArrayList has used to save the data
 /@iterator class used to get the content from the object one by one
 */
 public class AddressBook {
     //main method executed
     public static void main(String[] args) {
-        System.out.println("WELCOME IN ADDRESS BOOK PROGRAM");
-        ArrayList<Contact_info> addContacts = new ArrayList<>();
-        //Scanner class is used to take input from user
-        Scanner sc = new Scanner(System.in);
-        Scanner s = new Scanner(System.in);
+        //System.out.println("WELCOME IN ADDRESS BOOK PROGRAM");
+        String firstName=null;
+        String lastName=null;
+        String email=null;
+        long phone_no=0;
+        String city=null;
+        String state=null;
+        int zip=0;
+        Contact_info contact_info=new Contact_info(firstName, lastName, email, phone_no,city, state, zip);
+        ArrayList<Contact_info> contactsAdd=new ArrayList<>();
+
+        Scanner userInput=new Scanner(System.in);
 
 
         int choice;
@@ -22,88 +28,22 @@ public class AddressBook {
             System.out.println("2: DISPLAY");
             System.out.println("3:EDIT");
             System.out.println("4:DELETE");
-            System.out.println("0: EXIT");
+
             System.out.println("ENTER YOUR CHOICE :");
-            choice = s.nextInt();
+            choice = userInput.nextInt();
             //switch statements are used to choose a perticular oparation
             switch (choice) {
                 case 1:
-                    System.out.println("Enter Name: ");
-                    String name = sc.nextLine();
-
-                    System.out.println("Enter Email: ");
-                    String email = sc.nextLine();
-                    System.out.println("Enter Phone Number: ");
-                    long phone = sc.nextLong();
-                    sc.nextLine();
-                    System.out.println("Enter City: ");
-                    String city = sc.nextLine();
-                    System.out.println("Enter State: ");
-                    String state = sc.nextLine();
-                    System.out.println("Enter Zip Pin: ");
-                    int zip = sc.nextInt();
-                    //addContact.add function is used to add the object type data to the ArrayList object
-                    addContacts.add(new Contact_info(name, email, phone, city, state, zip));
+                    contact_info.addContact();
                     break;
                 case 2:
-                    Iterator<Contact_info> display = addContacts.iterator();
-                    while (display.hasNext()) {
-                        Contact_info show = display.next();
-                        System.out.println(show);
-                    }
+                    contact_info.display();
                     break;
                 case 3:
-                    boolean found = false;
-                    Iterator<Contact_info> edit = addContacts.iterator();
-                    System.out.println("Enter the Name who's details you want to edit");
-                    name = sc.nextLine();
-                    while (edit.hasNext()) {
-                        Contact_info newdata = edit.next();
-                        if (newdata.name == name) {
-                            int index = addContacts.indexOf(newdata);
-                            System.out.println("ENTER THE NEW EMAIL ID");
-                            email = sc.nextLine();
-
-                            System.out.println("ENTER THE NEW PHONE NUMBER");
-                            phone = sc.nextInt();
-
-                            System.out.println("ENTER THE NEW CITY");
-                            city = sc.nextLine();
-
-                            System.out.println("ENTER THE NEW STATE");
-                            state = sc.nextLine();
-
-                            System.out.println("ENTER THE NEW ZIP CODE");
-                            zip = sc.nextInt();
-
-                            addContacts.set(index,new Contact_info(name, email, phone, city, state, zip));
-
-                            found = true;
-                        }
-                    }
-                    if (!found) {
-                        System.out.println("not found");
-                    } else
-                        System.out.println("edited successfully");
+                    contact_info.edit();
                     break;
                 case 4:
-                    boolean deleteItemFound=false;
-                    Iterator <Contact_info> delete=addContacts.iterator();
-
-                    System.out.println("ENTER THE NAME TO DELETE");
-                    String nameSearch=sc.nextLine();
-                    while(delete.hasNext()){
-                        Contact_info dataDel=delete.next();
-                        if(nameSearch.equals(dataDel.name)){
-                            delete.remove();
-                            deleteItemFound=true;
-                        }
-                    }
-                    if(!deleteItemFound){
-                        System.out.println("not found");
-                    }
-                    else
-                        System.out.println("deleted successfully");
+                    contact_info.delete();
                     break;
             }
         }
